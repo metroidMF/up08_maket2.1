8fde22e3b08ad9dfd511138343545daf9401edd1
metroidMF <165372980+metroidMF@users.noreply.github.com>
2024-12-24 19:21:42 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,35 +3,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const prevButton = document.querySelector(".prev__button");
   const nextButton = document.querySelector(".next__button");
   const slides = Array.from(slider.querySelectorAll(".burger"));
-  const slideCount = slides.length;
-  let slideIndex = 0;
-
-  prevButton.addEventListener("click", showPreviousSlide);
-  nextButton.addEventListener("click", showNextSlide);
-  function showPreviousSlide() {
-    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-    updateSlider();
-  }
-  function showNextSlide() {
-    slideIndex = (slideIndex + 1) % slideCount;
-    updateSlider();
-  }
-  function updateSlider() {
-    slides.forEach((slide, index) => {
-      if (index === slideIndex) {
-        slide.style.display = "flex";
-      } else {
-        slide.style.display = "none";
-      }
-    });
-
-    updateSlider();
-  }
-
   const burgerMenu = document.querySelector(".menu");
   const menu = document.querySelector(".header");
+
+  let slideIndex = 0;
+
+  function showPreviousSlide() {
+    if (window.innerWidth > 720) return;
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    updateSlider();
+  }
+
+  function showNextSlide() {
+    if (window.innerWidth > 720) return;
+    slideIndex = (slideIndex + 1) % slides.length;
+    updateSlider();
+  }
+
+  function updateSlider() {
+    slides.forEach((slide, index) => {
+      slide.style.display = index === slideIndex ? "flex" : "none";
+    });
+  }
 
   burgerMenu.addEventListener("click", () => {
     menu.classList.toggle("active");
   });
+
+  prevButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
 });
